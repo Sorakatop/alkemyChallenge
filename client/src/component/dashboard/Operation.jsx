85dc5189a554c14db7024999b935813f000cfaa5
sorakatop <alcocergonzalomatias@gmail.com>
2022-02-16 18:59:56 -0300

    operation form

--- client/src/component/dashboard/Operation.jsx
+++ client/src/component/dashboard/Operation.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
-
+import check from '../../icons/clipboard-check.svg'
 
 const Operation = () => {
   return (
-    <div>Operation</div>
+    <div>
+        Operaciones
+        <form>
+        <table>
+            <tbody>          
+              <tr>
+                <th>Monto</th> 
+                <th>Categoria</th>
+                <th>Tipo de Operacion</th>
+                <th>Concepto</th> 
+                <th>Guardar</th>          
+              </tr>
+            <tr>
+                <td>
+                    <input type="text" placeholder="$"/>
+                </td>
+                <td>
+                    <select>
+                        <option>Ingreso</option>
+                        <option>Egreso</option>
+                    </select>
+                </td>
+                <td>
+                    <input type="text" placeholder="Tipo de gasto"/>
+                </td>
+                <td>
+                    <input type="text" placeholder="En concepto de"/>
+                </td>
+                <td>
+                    <button type="submit"><img src={check}/></button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+      </form>
+    </div>
   )
 }
 
